8b44fcb85978586526c1bef4f233d99525af9ac1
satya karthik ramachandrapurapu <40475023+caffeine-coder1@users.noreply.github.com>
2025-01-01 10:26:08 +0000

    initial commit

---
+++ loops.js
@@ -0,0 +1,21 @@
+// for loop in Javascript
+
+for (idx = 0; idx < 10; idx += 2) {
+    console.log(idx)
+}
+
+// while loop
+idx = 20;
+
+while(idx < 30){
+    console.log(idx);
+    idx +=3;
+}
+
+idx = 0
+
+// do while loop
+do {
+    console.log(idx);
+    idx += 4
+} while (idx < 0)
